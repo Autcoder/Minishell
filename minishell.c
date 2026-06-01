8574dd7e32c1ec90fd1f29bb0418081cc5fb9b16
Autcoder <150786670+Autcoder@users.noreply.github.com>
2026-06-01 16:28:08 +0200

    change minishell.c

--- minishell.c
+++ minishell.c
@@ -22,53 +22,16 @@ char	*get_any(char *env[], char *path)
 			return(NULL);
         while (env[i])
         {
-                j = 0;
-                while (path[j] == env[i][j])
-                        j++;
-                if (!path[j])
-                        break ;
-                i++;
+			j = 0;
+			while (path[j] == env[i][j])
+					j++;
+			if (!path[j])
+					break ;
+			i++;
         }
 		if (env[i])
-        	return ((env[i] + (strlen(path) + 1)));
+        	return ((env[i] + (ft_strlen(path) + 1)));
 		return (NULL);
-}
-
-int	ft_isalnum(int c)
-{
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
-		|| (c >= '0' && c <= '9'))
-		return (42);
-	return (0);
-}
-
-int	ft_strlen(const char *str)
-{
-	char	*temp;
-
-	temp = (char *)str;
-	while (*str)
-		str++;
-	return ((int)(str - temp));
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*str;
-	int		len;
-	char	*ptr;
-
-	len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
-	str = (char *)malloc(len + 1);
-	if (!str)
-		return (NULL);
-	ptr = str;
-	while (*s1)
-		*ptr++ = *s1++;
-	while (*s2)
-		*ptr++ = *s2++;
-	*ptr = '\0';
-	return (str);
 }
 
 char	*ft_strnjoin(char const *s1, char const *s2, int size)
@@ -90,82 +53,6 @@ char	*ft_strnjoin(char const *s1, char const *s2, int size)
 		*ptr++ = *s2++;
 	*ptr = '\0';
 	return (str);
-}
-
-char	*ft_strdup(const char *src)
-{
-	int		i;
-	char	*dest;
-
-	i = 0;
-	while (src[i])
-		i++;
-	dest = malloc(sizeof(char) * i + 1);
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = ft_strlen(src);
-	if (size == 0)
-		return (j);
-	while (src[i] && i < size - 1)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (j);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s != (unsigned char)c && *s)
-		s++;
-	if (*s == '\0' && (unsigned char)c != '\0')
-		return (NULL);
-	return ((char *)s);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	char	*sub;
-	size_t	length_of_s;
-
-	length_of_s = ft_strlen(s);
-	if (length_of_s < start)
-	{
-		sub = malloc(1);
-		if (!sub)
-			return (NULL);
-		return (sub[0] = '\0', sub);
-	}
-	if ((start + len) > length_of_s)
-		len = length_of_s - start;
-	sub = malloc(sizeof(char) * len + 1);
-	if (!sub)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		sub[i] = s[start + i];
-		i++;
-	}
-	return (sub[i] = '\0', sub);
 }
 
 char	**parse_env_to_dict()
@@ -250,15 +137,15 @@ int	main(int argc, char **argv, char *env[])
 	char	*test;
 	while (42)
 	{
-	str = readline("minishell>");
-	char *exit = "exit";
-	char *cpy_str = str;
-	while (*cpy_str++ == *exit)
-		if (!*exit++)
-			return (--exit, free(str), free(test), 0);
-	test = scan_expand(str);
-	printf("%s\n", test);
-	//free(str);
-	//free(test);
+		str = readline("minishell>");
+		char *exit = "exit";
+		char *cpy_str = str;
+		while (*cpy_str++ == *exit)
+			if (!*exit++)
+				return (--exit, free(str), free(test), 0);
+		test = scan_expand(str);
+		printf("%s\n", test);
+		//free(str);
+		//free(test);
 	}
 }
